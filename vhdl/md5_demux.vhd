8a1860a670e432f29c2d46ae04f9f8ec51e0bf1a
Niklas Aldén <niklas.alden1@gmail.com>
2014-09-22 17:57:10 +0200

    Added DEMUX and updated pkg and MUX

--- vhdl/md5_demux.vhd
+++ vhdl/md5_demux.vhd
@@ -3,7 +3,7 @@
 -- 
 -- Module Name:    md5_mux - Behavioral 
 -- Description: 
---			A mux to select which hash to compare
+--			A demux to select which md5 to use for hashing
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -12,23 +12,23 @@ use IEEE.NUMERIC_STD.ALL;
 -- include the hash_array type --
 use work.hash_array_pkg.all;
 
-entity md5_mux is
+entity md5_demux is
     generic (
         N : integer
     );
     port (
-        i_hash : in hash_array(N-1 downto 0);
-        i_select : in std_logic_vector(N-1 downto 0); -- should be ceil(log2(N-1))
+			i_md5_indata : in md5_indata_t;
+			i_select : in std_logic_vector(N-1 downto 0); -- should be ceil(log2(N-1))
         
-        o_hash : out std_logic_vector(127 downto 0)
+			o_md5_indata : out md5_indata_t_array(N-1 downto 0)
     );
-end md5_mux;
+end md5_demux;
 
-architecture Behavioral of md5_mux is
+architecture Behavioral of md5_demux is
 
 begin
 
-	o_hash <= i_hash(to_integer(unsigned(i_select)));
+	o_md5_indata(to_integer(unsigned(i_select))) <= i_md5_indata;
 
 end Behavioral;
 
